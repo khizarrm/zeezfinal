0a084ec6ea978e270b9192b8dd2b77cd6fa600ec
Khizar Malik <khizarmalik2003@gmail.com>
2025-05-04 17:29:36 +0200

    image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 30,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 30,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 30,
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
